2d94018c79d640f0cb5957a88889c5ec045bc122
Maciej Miszczyk <maciejmmiszczyk@gmail.com>
2023-05-26 16:21:08 +0200

    Update shell2http.py

--- shell2http.py
+++ shell2http.py
@@ -4,9 +4,6 @@ from argparse import ArgumentParser
 from urllib.parse import urlparse, parse_qs
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-index_page = '<!DOCTYPE html><html><head><title>Index</title><style>a{display:block;margin:16px}</style></head><body>%s</body></html>'
-
-
 def shellHTTPRequestHandler():
 
     class ShellHTTPRequestHandler(BaseHTTPRequestHandler):
